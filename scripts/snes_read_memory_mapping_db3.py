71768b8901573ef315655118b2f41e513ac43233
devilschile2 <106693100+devilschile2@users.noreply.github.com>
2023-04-05 11:46:40 +0200

    Add files via upload

--- scripts/snes_read_memory_mapping_db3.py
+++ scripts/snes_read_memory_mapping_db3.py
@@ -5,6 +5,7 @@ import sys
 
 def get_mapping_type(filename):
     # open the file in binary mode
+    print (filename)
     with open(filename, 'rb') as f:
         
         # read the first byte of the file to get the mapping type
@@ -12,20 +13,25 @@ def get_mapping_type(filename):
         
         # extract the mapping type from the byte using bitwise operations
         mapping_type = ((mapping_type_byte[0] & 0x0F) >> 2) | ((mapping_type_byte[0] & 0x10) >> 3)
-        
+        banks=None
         # print the mapping type as a string
         if mapping_type == 0:
             mapping_type_str = 'LoROM'
+            banks='up to 4MB, banks $00, $7D at address $8000 - $FFFF'
         elif mapping_type == 1:
             mapping_type_str = 'HiROM'
+            banks='up to 4MB, banks  $40 address $0000 - $FFFF'
         elif mapping_type == 2:
             mapping_type_str = 'ExLoROM'
+            banks='up to 16MB $40 -7D:8000-FFFF '
         elif mapping_type == 3:
             mapping_type_str = 'ExHiROM'
+            banks='up to 16MB $40 -7D:8000-FFFF '
         else:
             mapping_type_str = 'Unknown mapping type'
+            banks='unknown'
         
-        return mapping_type_str
+    return mapping_type_str,str(mapping_type_byte), banks 
 
 
 if len(sys.argv) < 2:
@@ -47,12 +53,13 @@ file_list = os.listdir(directory)
 # write the filename and mapping type of each SFC file to a CSV file
 with open('mapping_types.csv', 'w', newline='') as csvfile:
     csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['Filename', 'Mapping Type'])
-    for root, dirs, files in os.walk(folder_path):    
+    csvwriter.writerow(['Filename', 'Mapping Type','Address info'])
+    for root, dirs, files in os.walk(directory):    
         for filename in files:
-            full_filename = os.path.join(directory, filename)
-            mapping_type = get_mapping_type(full_filename)
-            csvwriter.writerow([filename, mapping_type])
+            if filename.lower().endswith(".sfc"):
+                full_filename = os.path.join(directory, filename)
+                mapping_type,byte_value,banks= get_mapping_type(full_filename)
+                csvwriter.writerow([filename, byte_value,mapping_type,banks])
 
 print('Done!')
 
